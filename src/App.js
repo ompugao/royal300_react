2566d69b86baa83ff072243fd719ac69c65dd4d2
Shohei Fujii <fujii.shohei@gmail.com>
2019-03-21 16:05:07 +0900

    wip

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Carousel from 'nuka-carousel';
+
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {english_scripts: []}
+  }
+  loadData() {
+    console.log(window.location.href)
+    fetch(window.location.href + '/royal-300.json')
+      .then(response => response.json())
+      .then(data =>{
+        this.setState({english_scripts: data})
+      })
+      .catch(err => console.error(this.props.url, err.toString()))
+  }
+  componentDidMount() {
+    this.loadData()
+  }
+
   render() {
     return (
       <div className="App">
+        {/*
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            <img src={logo} className="App-logo" alt="logo" />
         </header>
+        */}
+        <Carousel className="App-header">
+            <div>
+            </div>
+        </Carousel>
       </div>
     );
   }
