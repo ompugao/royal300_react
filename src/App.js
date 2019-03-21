cb03d80be61540bf60281d0ae5e1e4fda8d2af60
Shohei Fujii <fujii.shohei@gmail.com>
2019-03-21 16:32:33 +0900

    wip

--- src/App.js
+++ src/App.js
@@ -4,6 +4,53 @@ import Carousel from 'nuka-carousel';
 // import logo from './logo.svg';
 import './App.css';
 
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+ 
+const cardstyles = {
+  card: {
+    minWidth: 275,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+};
+
+function MyCard(props) {
+  const { s } = props;
+  return (
+    <Card className={s.no}>
+      <CardContent>
+        <Typography variant="h5" component="h2">
+          {s.english}
+        </Typography>
+        <Typography component="p">
+          {s.japanese}
+        </Typography>
+        <Typography component="p">
+          {s.note}
+        </Typography>
+      </CardContent>
+      {/*
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+      */}
+    </Card>
+  );
+}
+
 class App extends Component {
   constructor(props) {
     super(props)
@@ -22,6 +69,7 @@ class App extends Component {
     this.loadData()
   }
 
+
   render() {
     return (
       <div className="App">
@@ -30,9 +78,13 @@ class App extends Component {
             <img src={logo} className="App-logo" alt="logo" />
         </header>
         */}
+        var cards = [];
+        for(var i in this.state.english_scripts) {
+          cards.push(<MyCard s={this.state.english_scripts[i]} />);
+        }
+
         <Carousel className="App-header">
-            <div>
-            </div>
+          {cards}
         </Carousel>
       </div>
     );
